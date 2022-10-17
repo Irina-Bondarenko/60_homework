4f4f74568fb60fc630ed7c71337cc04b93775baa
Irina-Bondarenko <96590953+Irina-Bondarenko@users.noreply.github.com>
2022-10-17 22:31:31 +0300

    init

---
+++ postcatalog/src/App.js
@@ -0,0 +1,11 @@
+import { PostCatalog } from "./components/PostCatalog";
+
+function App() {
+  return (
+    <div className="App">
+      <PostCatalog />
+    </div>
+  );
+}
+
+export default App;
